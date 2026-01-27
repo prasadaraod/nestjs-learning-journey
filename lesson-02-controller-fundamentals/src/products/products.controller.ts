9e65f02960f61a2ba3cd93664ab4e71d079c96bb
prasada rao darla <prasadarao.darla@gmail.com>
2026-01-27 18:38:52 +0530

    added Get, Post , Put, Delete methods to products controller

--- lesson-02-controller-fundamentals/src/products/products.controller.ts
+++ lesson-02-controller-fundamentals/src/products/products.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    @Get()
+    getAllProducts() :any {
+        return { message:" get all products" };
+    }
+
+    @Post()
+    createProduct(@Body() taskData: any): any {
+        return {
+            message: "Task created Success fully",
+            data: taskData
+        };
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') id:string): any {
+        return `this return task by id ${id}`;
+    }
+
+    @Put(':id')
+    updateProduct(@Param('id') id:string, @Body() updateData: any):any {
+        return {
+            message: `Product ${id} successfully updated`,
+            updateData: updateData
+        };
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id') id:string) : any {
+        return { message: `Product ${id} deleted Successfully`}
+    }
+}
